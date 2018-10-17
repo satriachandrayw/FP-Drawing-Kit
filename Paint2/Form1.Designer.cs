1bd13921c30e9aba8c15bbfc012f356023e9ec2f
satriachandrayw <31718886+satriachandrayw@users.noreply.github.com>
2018-10-17 13:27:19 +0700

    Commit pertama

---
+++ Paint2/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Paint_sederhana
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.line = new System.Windows.Forms.Button();
+            this.rectang = new System.Windows.Forms.Button();
+            this.circle = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.refresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // line
+            // 
+            this.line.Location = new System.Drawing.Point(13, 15);
+            this.line.Name = "line";
+            this.line.Size = new System.Drawing.Size(75, 23);
+            this.line.TabIndex = 0;
+            this.line.Text = "Line";
+            this.line.UseVisualStyleBackColor = true;
+            this.line.Click += new System.EventHandler(this.line_Click);
+            // 
+            // rectang
+            // 
+            this.rectang.Location = new System.Drawing.Point(94, 15);
+            this.rectang.Name = "rectang";
+            this.rectang.Size = new System.Drawing.Size(89, 23);
+            this.rectang.TabIndex = 1;
+            this.rectang.Text = "Rectangle";
+            this.rectang.UseVisualStyleBackColor = true;
+            this.rectang.Click += new System.EventHandler(this.rectang_Click);
+            // 
+            // circle
+            // 
+            this.circle.Location = new System.Drawing.Point(189, 15);
+            this.circle.Name = "circle";
+            this.circle.Size = new System.Drawing.Size(75, 23);
+            this.circle.TabIndex = 2;
+            this.circle.Text = "Circle";
+            this.circle.UseVisualStyleBackColor = true;
+            this.circle.Click += new System.EventHandler(this.circle_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Location = new System.Drawing.Point(13, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(673, 424);
+            this.panel1.TabIndex = 3;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick_1);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown_1);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove_1);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // refresh
+            // 
+            this.refresh.Location = new System.Drawing.Point(611, 15);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(75, 23);
+            this.refresh.TabIndex = 4;
+            this.refresh.Text = "Refresh";
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 480);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.circle);
+            this.Controls.Add(this.rectang);
+            this.Controls.Add(this.line);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button line;
+        private System.Windows.Forms.Button rectang;
+        private System.Windows.Forms.Button circle;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button refresh;
+    }
+}
+
---
+++ Paint2/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Paint_sederhana
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Graphics Gambar;
+        private bool MyPic = false;
+        private int curX, curY, x, y, showX, showY;
+        private double length;
+        private int bentuk = 0, ee = 0;
+        //private Color warna;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Gambar = panel1.CreateGraphics();
+        }
+
+        private void line_Click(object sender, EventArgs e)
+        {
+            bentuk = 1;
+            line.BackColor = Color.Red;
+            circle.BackColor = Color.White;
+            rectang.BackColor = Color.White;
+        }
+
+        private void rectang_Click(object sender, EventArgs e)
+        {
+            bentuk = 2;
+            rectang.BackColor = Color.Red;
+            circle.BackColor = Color.White;
+            line.BackColor = Color.White;
+        }
+
+        private void circle_Click(object sender, EventArgs e)
+        {
+            bentuk = 3;
+            circle.BackColor = Color.Red;
+            line.BackColor = Color.White;
+            rectang.BackColor = Color.White;
+        }
+
+        /*private void pilih_warna_Click(object sender, EventArgs e)
+        {
+            ColorDialog col = new ColorDialog();
+            if (col.ShowDialog() == DialogResult.OK)
+            {
+                pilih_warna.BackColor = col.Color;
+                warna = col.Color;
+            }
+        }*/
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            this.Refresh();
+            //textBox1.Text = " ";
+            //textBox2.Text = " ";
+            //textBox3.Text = " ";
+        }
+
+        private void panel1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                MyPic = true;
+                ee++;
+            }
+
+            curX = e.X;
+            curY = e.Y;
+        }
+
+        private void panel1_MouseClick_1(object sender, MouseEventArgs e)
+        {
+            if (MyPic == true)
+            x = e.X;
+            y = e.Y;
+            showX = e.X - curX;
+            showY = e.Y - curY;
+
+            if (bentuk == 1)
+            {
+                Gambar.DrawLine(new Pen(Color.Black), curX, curY, e.X, e.Y);
+                Gambar.DrawLine(new Pen(Color.Red), curX, curY, e.X, e.Y);
+            }
+            if (bentuk == 2)
+            {
+                Gambar.DrawRectangle(new Pen(Color.Black), curX, curY, showX, -showY);
+                Gambar.DrawRectangle(new Pen(Color.Red), curX, curY, showX, -showY);
+            }
+            if (bentuk == 3)
+            {
+                Gambar.DrawEllipse(new Pen(Color.Black), curX, curY, showX, -showY);
+                Gambar.DrawEllipse(new Pen(Color.Red), curX, curY, showX, -showY);
+            }
+        }
+
+        private void panel1_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            length = Math.Sqrt((showX * showX) + (showY * showY));
+            //TextBox1.Text = Convert.ToString(showX);
+            //TextBox2.Text = Convert.ToString(showY);
+            //TextBox3.Text = Convert.ToString(length);
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            MyPic = false;
+        }
+    }
+}
