be3d77349003b3f22777a56be961e2188af58c66
satriachandrayw <31718886+satriachandrayw@users.noreply.github.com>
2018-12-05 16:58:54 +0700

    Debug dan merapihkan struktur code

--- Paint2/Form1.cs
+++ Paint2/Form1.cs
@@ -5,7 +5,9 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace Paint_sederhana
 {
@@ -16,16 +18,97 @@ namespace Paint_sederhana
             InitializeComponent();
         }
 
+        Pen pen;
         private Graphics Gambar;
+        private Point preCor, newCor;
         private bool MyPic = false;
         private int curX, curY, x, y, showX, showY;
+        int cirW, cirL;
         private double length;
         private int bentuk = 0, ee = 0;
+        private const double EPSILON = 3.0;
         //private Color warna;
+
+        class Line
+        {
+            public Point from, to;
+
+            public bool Intersect(int xTest, int yTest)
+            {
+                double m = GetSlope();
+                double b = to.Y - m * to.X;
+                double y_point = m * xTest + b;
+
+                if (Math.Abs(yTest - y_point) < EPSILON)
+                {
+                    System.Diagnostics.Debug.WriteLine("Terpilih");
+                    return true;
+                }
+                return false;
+            }
+
+            public double GetSlope()
+            {
+                double m = (double)(to.Y - from.Y) / (double)(to.X - from.X);
+                return m;
+            }
+        }
+
+        class Circle
+        {
+            public int xFrom, yFrom;
+            public Point to;
+        }
+
+        class Rectang
+        {
+            public int xFrom, yFrom;
+            public Point to;
+        }
+
+        List<Line> listLine = new List<Line>();
+        List<Circle> listCircle = new List<Circle>();
+        List<Rectang> listRectang = new List<Rectang>();
+        List<Stack> listUndo = new List<Stack>();
+
+        class Stack
+        {
+            public String bentuk;
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             Gambar = panel1.CreateGraphics();
+
+            foreach (Line line in listLine)
+            {
+                Gambar.DrawLine(pen, line.from, line.to);
+            }
+
+            foreach (Circle circle in listCircle)
+            {
+                p.Width = 2;
+                cirW = Math.Abs(circle.to.X - circle.xFrom);
+                cirL = Math.Abs(circle.to.Y - circle.yFrom);
+                Rectangle rec = new Rectangle(Math.Min(circle.to.X, circle.xFrom),
+                Math.Min(circle.to.Y, circle.yFrom),
+                Math.Abs(circle.to.X - circle.xFrom),
+                Math.Abs(circle.to.Y - circle.yFrom));
+                Gambar.DrawEllipse(pen, rec);
+            }
+
+            foreach (Rectang rectang in listRectang)
+            {
+                p.Width = 2;
+                int width = rectang.to.X - rectang.xFrom;
+                int height = rectang.to.Y - rectang.yFrom;
+                Rectangle rect = new Rectangle(Math.Min(rectang.to.X, rectang.xFrom),
+                Math.Min(rectang.to.Y, rectang.yFrom),
+                Math.Abs(rectang.to.X - rectang.xFrom),
+                Math.Abs(rectang.to.Y - rectang.yFrom));
+                newCoor = new Point(rectang.to.X, rectang.to.Y);
+                Gambar.DrawRectangle(pen, rect);
+            }
         }
 
         private void line_Click(object sender, EventArgs e)
@@ -88,7 +171,7 @@ namespace Paint_sederhana
             x = e.X;
             y = e.Y;
             showX = e.X - curX;
-            showY = e.Y - curY;
+            showY = curY - e.Y;
 
             if (bentuk == 1)
             {
@@ -98,12 +181,12 @@ namespace Paint_sederhana
             if (bentuk == 2)
             {
                 Gambar.DrawRectangle(new Pen(Color.Black), curX, curY, showX, -showY);
-                Gambar.DrawRectangle(new Pen(Color.Red), curX, curY, showX, -showY);
+                Gambar.DrawRectangle(new Pen(Color.Green), curX, curY, showX, -showY);
             }
             if (bentuk == 3)
             {
                 Gambar.DrawEllipse(new Pen(Color.Black), curX, curY, showX, -showY);
-                Gambar.DrawEllipse(new Pen(Color.Red), curX, curY, showX, -showY);
+                Gambar.DrawEllipse(new Pen(Color.Blue), curX, curY, showX, -showY);
             }
         }
 
